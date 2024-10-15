0a49091f2683fb402ca5591967f01c284fcb9ca8
Elmar van Riet <elmarvr@Elmars-MacBook-Pro.local>
2024-10-15 15:12:35 +0200

    And local client :/

--- app/lib/spotify.ts
+++
@@ -1,5 +0,0 @@
-import type { App } from "../../spotify-worker/src/index";
-import { hc } from "hono/client";
-import { Resource } from "sst";
-
-export const spotify = hc<App>(Resource.Spotify.url);
